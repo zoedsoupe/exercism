63fddacd05503109b845131bf707b68830c9b554
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2020-09-13 16:03:05 -0300

    armstring numbers and difference of squares fp

--- typescript/armstrong-numbers/armstrong-numbers.ts
+++ typescript/armstrong-numbers/armstrong-numbers.ts
@@ -1,15 +1,8 @@
-const ArmstrongNumbers = {
-  isArmstrongNumber: function (digit: number): boolean {
-    const numbers = `${digit}`.split("").map((num) => Number(num));
-    const power = numbers.length;
+const splitNumber = (x: number): readonly number[] =>
+  `${x}`.split('').map(Number);
 
-    const sum = numbers.reduce((acc, num) => {
-      return (acc += Math.pow(num, power));
-    }, 0);
+const sum = (nums: readonly number[]): number =>
+  nums.reduce((acc, num) => acc + Math.pow(num, nums.length), 0);
 
-    if (sum === digit) return true;
-
-    return false;
-  },
-};
-export default ArmstrongNumbers;
+export const isArmstrongNumber = (digit: number): boolean =>
+  sum(splitNumber(digit)) === digit ? true : false;
