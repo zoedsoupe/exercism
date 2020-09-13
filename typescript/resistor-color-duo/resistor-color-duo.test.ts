ab16e9e68ef9ce225450c265f278dfabbc74c2f1
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2020-09-13 17:29:01 -0300

    resistor-color-duo fp

--- typescript/resistor-color-duo/resistor-color-duo.test.ts
+++ typescript/resistor-color-duo/resistor-color-duo.test.ts
@@ -1,34 +1,29 @@
-import { ResistorColor } from "./resistor-color-duo";
+import { resistorColor } from './resistor-color-duo';
 
-describe("Resistor Colors", () => {
-  it("Brown and black", () => {
-    const resistorColor = new ResistorColor(["brown", "black"]);
-    expect(resistorColor.value()).toEqual(10);
+describe('Resistor Colors', () => {
+  it('Brown and black', () => {
+    expect(resistorColor(['brown', 'black'])).toEqual(10);
   });
 
-  it("Blue and grey", () => {
-    const resistorColor = new ResistorColor(["blue", "grey"]);
-    expect(resistorColor.value()).toEqual(68);
+  it('Blue and grey', () => {
+    expect(resistorColor(['blue', 'grey'])).toEqual(68);
   });
 
-  it("Yellow and violet", () => {
-    const resistorColor = new ResistorColor(["yellow", "violet"]);
-    expect(resistorColor.value()).toEqual(47);
+  it('Yellow and violet', () => {
+    expect(resistorColor(['yellow', 'violet'])).toEqual(47);
   });
 
-  it("Orange and orange", () => {
-    const resistorColor = new ResistorColor(["orange", "orange"]);
-    expect(resistorColor.value()).toEqual(33);
+  it('Orange and orange', () => {
+    expect(resistorColor(['orange', 'orange'])).toEqual(33);
   });
 
-  it("Ignore additional colors", () => {
-    const resistorColor = new ResistorColor(["green", "brown", "orange"]);
-    expect(resistorColor.value()).toEqual(51);
+  it('Ignore additional colors', () => {
+    expect(resistorColor(['green', 'brown', 'orange'])).toEqual(51);
   });
 
-  it("Throws error when not enough colors", () => {
-    expect(() => new ResistorColor(["green"])).toThrowError(
-      "At least two colors need to be present"
+  it('Throws error when not enough colors', () => {
+    expect(resistorColor(['green'])).toEqual(
+      'At least two colors need to be present'
     );
   });
 });
--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -13,19 +13,12 @@ const COLOR_TO_RESISTOR_VALUES = {
 
 type Color = keyof typeof COLOR_TO_RESISTOR_VALUES;
 
-export class ResistorColor {
-  private readonly MAX_BAND_NUMBER = 2;
-  private readonly colors: Color[];
+const getColorValue = (colors: readonly Color[]): number =>
+  colors
+    .map((color) => COLOR_TO_RESISTOR_VALUES[color])
+    .reduce((acc, value) => acc * 10 + value);
 
-  constructor(colors: Color[]) {
-    if (colors.length < this.MAX_BAND_NUMBER)
-      throw new Error("At least two colors need to be present");
-    else this.colors = colors.slice(0, this.MAX_BAND_NUMBER);
-  }
-
-  value(): number {
-    return this.colors
-      .map((color) => COLOR_TO_RESISTOR_VALUES[color])
-      .reduce((acc, value) => acc * 10 + value);
-  }
-}
+export const resistorColor = (colors: readonly Color[]): string | number =>
+  colors.length < 2
+    ? 'At least two colors need to be present'
+    : getColorValue(colors.slice(0, 2));
