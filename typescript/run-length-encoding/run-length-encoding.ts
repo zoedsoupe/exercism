fea4ef73f51689b01abacbdb63456b7bff600669
Zoey <zoey.spessanha@zeetech.io>
2020-09-13 17:36:24 -0300

    run-length-encoding fp

--- typescript/run-length-encoding/run-length-encoding.ts
+++ typescript/run-length-encoding/run-length-encoding.ts
@@ -1,12 +1,10 @@
-export default {
-  encode: (input: string): string => {
-    return input.replace(/([a-z]|[A-Z]| )\1+/g, (matched, char: string) => {
-      return matched.length + char;
-    });
-  },
-  decode: (input: string): string => {
-    return input.replace(/(\d+)(\w| )/g, (_, rep: number, char: string) => {
-      return char.repeat(rep);
-    });
-  },
-};
+export const encode = (input: string): string =>
+  input.replace(
+    /([a-z]|[A-Z]| )\1+/g,
+    (matched, char: string) => matched.length + char
+  );
+
+export const decode = (input: string): string =>
+  input.replace(/(\d+)(\w| )/g, (_, rep: number, char: string) =>
+    char.repeat(rep)
+  );
