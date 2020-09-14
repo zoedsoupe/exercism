cd33aecc68b142362cc09152ebdd45c8ba306c96
Zoey <zoey.spessanha@zeetech.io>
2020-09-13 22:14:55 -0300

    improved strain

--- typescript/strain/strain.ts
+++ typescript/strain/strain.ts
@@ -1,8 +1,12 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export const keep = <T>(arr: readonly T[], fn: (e: any) => any): readonly T[] =>
-  arr.filter(fn);
+type Predicate<T> = (e: T) => boolean;
+
+export const keep = <T>(arr: readonly T[], pred: Predicate<T>): readonly T[] =>
+  arr.reduce(
+    (result, el) => (pred(el) ? [...result, el] : result),
+    [] as readonly T[]
+  );
 
 export const discard = <T>(
   arr: readonly T[],
-  fn: (e: any) => any
-): readonly T[] => arr.filter((e) => !fn(e));
+  pred: Predicate<T>
+): readonly T[] => keep(arr, (e) => !pred(e));
